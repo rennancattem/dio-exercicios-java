d4ba249685c89fe602d7f9167dbc3439341388e0
cattem <cattemsud@gmail.com>
2022-11-21 08:35:32 -0300

    Projeto desafio de Padroes de Projeto

---
+++ Padroes-Projeto/src/main/java/com/dio/service/ClienteService.java
@@ -0,0 +1,22 @@
+package com.dio.service;
+
+import com.dio.model.Cliente;
+
+/**
+ * "Interface" que define o padrão Strategy no domínio de cliente.
+ * Com isso, se necessário, podemos ter multiplas implementações dessa mesma
+ * "interface"
+ */
+
+public interface ClienteService {
+
+    Iterable<Cliente> buscarTodos();
+
+    Cliente buscarPorId(Long id);
+
+    void inserir(Cliente cliente);
+
+    void atualizar(Long id, Cliente cliente);
+
+    void deletar(Long id);
+}
---
+++ Padroes-Projeto/src/test/java/com/dio/PadroesProjetoSpringApplicationTests.java
@@ -0,0 +1,12 @@
+package com.dio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PadroesProjetoSpringApplicationTests {
+
+    @Test
+    void contextLoads(){
+    }
+}
